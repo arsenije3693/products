8191ee7890d7ee2e24e084091b9a6318d7ba1247
Uriel Hernandez <urielhernandez6497@gmail.com>
2026-01-13 11:00:54 -0700

    fixed typo in OrdersController.java, typo was package name

--- src/main/java/edu/brajovic/products/controller/OrdersController.java
+++ src/main/java/edu/brajovic/products/controller/OrdersController.java
@@ -1,4 +1,4 @@
-package edu.brajovic.products.controllers;
+package edu.brajovic.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
